27e78036b9ff0df94452a2882b6b66e2f2c493bb
young <siosio34@nate.com>
2017-09-25 00:57:50 +0900

    Make First Login Activity UI

---
+++ app/src/main/java/com/b05studio/boxstore/auth/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.b05studio.boxstore.auth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.b05studio.boxstore.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
